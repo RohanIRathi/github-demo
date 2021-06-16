a7778a3e1545cb281083072816d2c7a50ea4a9f2
RohanIRathi <rathirohan8@gmail.com>
2021-06-16 15:02:32 +0530

    Added function headers

--- math.py
+++ math.py
@@ -1,23 +1,15 @@
 # Add Implementation
 def add(x,y):
-	return x+y
+	pass
 
 # Subtract implementation
 def subtract(x,y):
-	return x-y # On master branch
+	pass
 
 # Multiply implementation
 def multiply(x,y):
-	return x*y # On bug0004 branch
+	pass
 
 # Divide Implementation
 def divide(x,y):
-	if x<0: # On master branch
-		return "Error: Negative Parameter"
-	if y == 0: # On bug0005 branch
-		return "Error: Cannot divide by 0"
-	else:
-		return x/y
-
-def square(x):
-    return x**2
+	pass
